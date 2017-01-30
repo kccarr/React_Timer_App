6a47aae6454b53b4379f77b7a780b2e4b8b95ead
Kevin Carr <cochewen@gmail.com>
2017-01-30 18:19:33 -0500

    Added clock styles and testing for clock components

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,10 +1,10 @@
 var React = require('react');
-var {Link} = require('react-router');
+var Clock = require('Clock');
 
 var Countdown = (props) => {
   return (
     <div>
-      <h4>Countdown component</h4>
+      <Clock totalSeconds={129} />
     </div>
   );
 };
