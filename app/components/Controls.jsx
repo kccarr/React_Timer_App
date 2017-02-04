ca3feca67149a990c29c721f9717747e27925a4c
Kevin Carr <cochewen@gmail.com>
2017-02-04 12:49:04 -0500

    Add controls scss styles and functionality that paused-started and cleared clock timer

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -2,21 +2,29 @@ var React = require('react');
 
 var Controls = React.createClass({
   propTypes: {
-    countdownStatus: React.PropTypes.string.isRequired
+    countdownStatus: React.PropTypes.string.isRequired,
+    onStatusChange: React.PropTypes.func.isRequired
+  },
+  onStatusChange: function(newStatus) {
+    return () => {
+      this.props.onStatusChange(newStatus);
+    }
   },
   render: function() {
     var {countdownStatus} = this.props;
     var renderStartStopButton = () => {
       if (countdownStatus === 'started') {
-        return <button className="button secondary"><span className="rendered">Pause</span></button>
+        // onClick is using a currying pattern
+        // to use a function to return a different function
+        return <button className="button secondary" onClick={this.onStatusChange('paused')}><span className="rendered">Pause</span></button>
       } else if (countdownStatus === 'paused') {
-        return <button className="button primary">Start</button>
+        return <button className="button primary" onClick={this.onStatusChange('started')}>Start</button>
       }
     };
     return (
       <div className="controls">
         {renderStartStopButton()}
-        <button className="button alert hollow">Clear</button>
+        <button className="button alert hollow" onClick={this.onStatusChange('stopped')}>Clear</button>
       </div>
     );
   }
